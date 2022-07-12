16eecd60402babb21e19aaa7c5f2e8d994a8d492
jcariajano <juliocariajano@gmail.com>
2022-07-11 19:46:44 -0500

    07112022

--- client/src/components/Card.js
+++ client/src/components/Card.js
@@ -6,15 +6,16 @@ import StyledCard, { StyledLink } from '../Style/StyleCard';
 
 export default function Card({ id, imagen, name, continent, cca3 }) {
   return (
+<StyledLink to={`/home/${cca3}`}>
     <StyledCard>
 
     <div className="name">
-    <StyledLink to={`/home/${cca3}`}>
+    
           <h1 >{name} </h1>
           <div className="divIMG">
     <img className="mainIMG" src={imagen} alt="img not found" />
     </div> 
-        </StyledLink>
+        
     
      
     </div>
@@ -22,5 +23,6 @@ export default function Card({ id, imagen, name, continent, cca3 }) {
       <h4 >{continent}</h4>
     
     </StyledCard>
+    </StyledLink>
   );
 }
